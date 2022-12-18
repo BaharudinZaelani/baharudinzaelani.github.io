5075bddbc9310214132ed9c3ecef87ce80f9f9de
Zaelani <bahar32harz@gmail.com>
2022-12-18 22:28:53 +0700

    responsive design for blog post : Simseo Template

--- assets/js/blog/simseo.js
+++ assets/js/blog/simseo.js
@@ -3,6 +3,16 @@ $(document).ready(()=>{
     // get image
     let postImage = $(".post-image");
     postImage.each(function(e) {
-        $(this).css('background-image', 'url(' + $(this).children().attr('content') );
+        $(this).css('background-image', 'url(' + $(this).children().attr('content') + ')' );
     });
+
+    // blog post
+    // add clas to separator image
+    let separator = $(".separator");
+    separator.each(function(e) {
+        // tag img
+        $(this).children().children().css("width", "100%");
+        $(this).children().children().addClass("img-thumbnail");
+    });
+
 });
