e7aa45cdc93fd07edac04c122c8f4cca3a1d79f1
Zaelani <bahar32harz@gmail.com>
2023-08-21 14:37:23 +0700

    Update sementara

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,25 @@ $(window).scroll(function() {
     }
 });
 
+const templatePortofolia = `
+    <div class="portofolio">
+        <div class="img">
+            <img src="!image" class="img-fluid" height="180">
+        </div>
+        <div class="description">
+            <h2>!title</h2>
+            <p>!description</p>
+            <div>
+                <a target="_blank" class="d-inline-block btn btn-light btn-sm"
+                    href="!link">
+                    <i class="bi bi-link-45deg"></i>
+                    <span>Link Webiste</span>
+                </a>
+            </div>
+        </div>
+    </div>
+`
+
 $(document).ready(()=>{
 
 });
